5838922d4b1776f2f754f1e23efe494802622cc7
Dharmesh Patel <dp837533@gmail.com>
2022-05-14 10:05:53 +0530

    business

--- src/containers/about/our_leadership/index.jsx
+++ src/containers/about/our_leadership/index.jsx
@@ -7,14 +7,14 @@ import Data from "../../../data/business.json"
 import LeadershipImmersiveLearning from './LeadershipImmersiveLearning';
 
 const OurLeadership = () => {
-    const LeadershipData=Data.our_leadership
+  const LeadershipData = Data.our_leadership
   return (
-      <>
-          <LeadershipWhatWeDo data={LeadershipData[0]}/>
-          <LeadershipMission data={LeadershipData[1]}/>
-          <LeadershipImmersiveLearning data={LeadershipData[2]}/>
-          <LeadershipAboutUs data={LeadershipData[3]}/>
-          <LeadershipSlider/>
+    <>
+      <LeadershipWhatWeDo data={LeadershipData[0]} />
+      <LeadershipMission data={LeadershipData[1]} />
+      <LeadershipImmersiveLearning data={LeadershipData[2]} />
+      <LeadershipAboutUs data={LeadershipData[3]} />
+      <LeadershipSlider data={LeadershipData[4]} />
     </>
   )
 }
