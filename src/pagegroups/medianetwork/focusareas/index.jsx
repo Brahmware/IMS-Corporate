6f9a63fa911b74df7050de5751094762d50c428b
Dharmesh Patel <dp837533@gmail.com>
2022-05-18 19:08:26 +0530

    focus and areas page added

--- src/pagegroups/medianetwork/focusareas/index.jsx
+++ src/pagegroups/medianetwork/focusareas/index.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import FocusAreasOne from "../../../containers/focus-areas/container-1"
+import FocusAreasTwo from '../../../containers/focus-areas/container-2';
+import FocusAreasThree from '../../../containers/focus-areas/container-3';
+import FocusAreasFour from '../../../containers/focus-areas/container-4';
 
-const FocusAreas = () => {
+const FocusAreas = ({data}) => {
+  
+  const dataContainer1 = data && data.find(data => data.id === 'container_1').elements;
+  const dataContainer2 = data && data.find(data => data.id === 'container_2').elements;
+  const dataContainer3 = data && data.find(data => data.id === 'container_3').elements;
+  const dataContainer4 = data && data.find(data => data.id === 'container_4').elements;
   return (
-    <div>FocusAreas</div>
+    <div className="focus-areas-page">
+      <FocusAreasOne data={dataContainer1}/>
+      <FocusAreasTwo data={dataContainer2}/>
+      <FocusAreasThree data={dataContainer3}/>
+      <FocusAreasFour data={dataContainer4}/>
+   </div>
   )
 }
 
