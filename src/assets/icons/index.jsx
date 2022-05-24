443bbe22610452a35bb8ed1442eb63e311ffa0ce
Sanchayan Bhunia <sanchayan.bhunia@hotmail.com>
2022-05-24 17:51:33 +0200

    resize map added

--- src/assets/icons/index.jsx
+++ src/assets/icons/index.jsx
@@ -3,6 +3,8 @@ export { ReactComponent as RightarrowIcon } from './rightarrow.svg';
 export { ReactComponent as LeftarrowIcon } from './leftarrow.svg';
 export { ReactComponent as HamburgerIcon } from './hamburger.svg';
 export { ReactComponent as SearchIcon } from './search.svg';
+export { ReactComponent as ResizeIcon } from './resize.svg';
+
 
 // Language Icons
 export { ReactComponent as GlobeIcon } from './globe.svg';
--- src/components/map-component/index.jsx
+++ src/components/map-component/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import { ResizeIcon } from '../../assets/icons';
 import { useD3 } from '../../utils/useD3';
 import CountriesGeologyData from '../../data/countries.geology.json'
 import * as d3 from 'd3';
 
 const MapComponent = (props) => {
-
 
     const draw = (svg) => {
 
@@ -15,7 +15,7 @@ const MapComponent = (props) => {
             .scale(150)
             .center([0, 45]);
 
-        const geoPathProjection = d3.geoPath().projection(projection);
+        const path = d3.geoPath().projection(projection);
 
         function drawGlobe() {
 
@@ -23,12 +23,49 @@ const MapComponent = (props) => {
                 .data(CountriesGeologyData.features)
                 .enter().append("path")
                 .attr("class", "country")
-                .attr("d", geoPathProjection)
                 .attr("id", (d) => d.id)
+                .attr("d", path)
                 .on("click", clicked);
         }
 
+        d3.select("#resize-icon").on('click', resizeMap)
+
+        function resizeMap() {
+
+            var projection = d3.geoMercator()
+                .scale(150)
+                .center([0, 45]);
+
+            const path = d3.geoPath().projection(projection);
+            svg.selectAll("path")
+                .transition()
+                .duration(600)
+                .attr("d", path);
+        }
+
         function clicked(event, data) {
+
+            let bounds = path.bounds(data),
+                dx = bounds[1][0] - bounds[0][0],
+                dy = bounds[1][1] - bounds[0][1],
+                x = (bounds[0][0] + bounds[1][0]) / 2,
+                y = (bounds[0][1] + bounds[1][1]) / 2,
+                scale = Math.max(1, Math.min(8, 0.9 / Math.max(dx / width, dy / height))),
+                translate = [width / 2 - scale * x, height / 2 - scale * y];
+
+            projection.translate(translate).scale();
+
+            // Transition to the new projection
+
+            svg.selectAll("path")
+                .transition()
+                .duration(600)
+                .attr("d", path);
+
+
+            // Add point showing new projection centre
+        }
+        /* function clicked(event, data) {
             let bounds = geoPathProjection.bounds(data),
                 dx = bounds[1][0] - bounds[0][0],
                 dy = bounds[1][1] - bounds[0][1],
@@ -36,35 +73,24 @@ const MapComponent = (props) => {
                 y = (bounds[0][1] + bounds[1][1]) / 2,
                 scale = Math.max(1, Math.min(8, 0.9 / Math.max(dx / width, dy / height))),
                 translate = [width / 2 - scale * x, height / 2 - scale * y];
-
-            /* if (active.node() === this) return reset();
-            active.classed("active", false);
-            active = select(this).classed("active", true);
-          
-            var bounds = geoPathProjection.bounds(d),
-                dx = bounds[1][0] - bounds[0][0],
-                dy = bounds[1][1] - bounds[0][1],
-                x = (bounds[0][0] + bounds[1][0]) / 2,
-                y = (bounds[0][1] + bounds[1][1]) / 2,
-                scale = Math.max(1, Math.min(8, 0.9 / Math.max(dx / width, dy / height))),
-                translate = [width / 2 - scale * x, height / 2 - scale * y];
-          
-            svg.transition()
-                .duration(750)
-                // .call(zoom.translate(translate).scale(scale).event); // not in d3 v4
-                .call( zoom.transform, zoomIdentity.translate(translate[0],translate[1]).scale(scale) ); // updated for d3 v4 */
-        }
+        } */
 
 
 
 
-        return drawGlobe();
+        drawGlobe();
     }
 
     const svgRef = useD3(draw, []);
     return (
         <div className='map-component'>
-            <svg id='map' ref={svgRef} viewBox="0 0 960 500" preserveAspectRatio="xMidYMid meet" />
+            <svg id='map' ref={svgRef} viewBox="0 0 960 500" preserveAspectRatio="xMidYMid meet" >
+            </svg>
+            <div className="resize-icon-holder">
+                <div id="resize-icon">
+                    <ResizeIcon />
+                </div>
+            </div>
         </div>
     )
 }
