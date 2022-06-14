8550675dc74b9ebbdddb0150a32e1202b922e9c0
Sanchayan Bhunia <sanchayan.bhunia@hotmail.com>
2022-06-14 17:30:15 +0200

    megamenu fixed

--- src/pagegroups/megamenu/index.jsx
+++ src/pagegroups/megamenu/index.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import CopyrightComponent from '../../components/copyright';
 import LanguageComponent from '../../components/languagecomponent';
 import OurSocialLinks from '../../components/our-social-links';
 import SiteMap from '../../components/sitemap';
 import PrivacyPolicySitemapData from '../../data/privacy-policy-sitemap.json';
+import useResizeObserver from '../../utils/useResizableObserver';
 
 const MegaMenu = (props) => {
 
@@ -12,57 +13,67 @@ const MegaMenu = (props) => {
     const visitor_agreement = pages && pages.find(page => page.id === 'visitor_agreement');
     const privacy_notice = pages && pages.find(page => page.id === 'privacy_notice');
 
+    const megaMenuImageRef = useRef(null);
+    const imageDimensions = useResizeObserver(megaMenuImageRef);
+    const [imageStyle, setImageStyle] = useState({});
+
+    useEffect(() => {
+        imageDimensions && setImageStyle({width: imageDimensions.height * 0.57})
+    }, [imageDimensions])
 
     return (
         <div
             className={props.show ? "megamenu open" : "megamenu"}
             style={{ paddingTop: `${props.headerHeight}px` }}
         >
-            <div className="page-wrapper megamenu-page">
-                <div className="site-map-wrapper">
-                    <div className="container">
+            <div className="container h-100">
+                <div className="page-wrapper megamenu-page">
+                    <div className="site-map-wrapper">
                         <div className="site-map-container">
-                            <div className="megamenu-image" >
+                            <div className="megamenu-image" 
+                                ref={megaMenuImageRef}
+                                style={imageStyle}
+                            >
                                 <img src="/images/nav-image.png" alt="navimage" />
                             </div>
-                            <SiteMap onClickClose={props.onclose}/>
+                            <SiteMap onClickClose={props.onclose} />
                         </div>
                     </div>
-                </div>
-                <div className="megamenu-footer container">
-                    <div className="megamenu-footer-left">
-                        <span className="contactus">CONTACT US</span>
-                        <div className="socials"><OurSocialLinks /></div>
-                        <div className="copyright"><CopyrightComponent /></div>
-                    </div>
-                    <div className="megamenu-footer-right">
-                        <div />
-                        <div className="language-preference">
-                            <LanguageComponent />
+                    <div className="megamenu-footer">
+                        <div className="megamenu-footer-left">
+                            <span className="contactus">CONTACT US</span>
+                            <div className="socials"><OurSocialLinks /></div>
+                            <div className="copyright"><CopyrightComponent /></div>
                         </div>
-                        <div className="legals">
-                            <Link 
-                                to={visitor_agreement.path} 
-                                id={visitor_agreement.id}
-                                onClick={props.onclose}
-                            >
-                                <span
-                                    dangerouslySetInnerHTML={{
-                                        __html: visitor_agreement.page
-                                    }}
-                                />
-                            </Link>
-                            <Link 
-                                to={privacy_notice.path} 
-                                id={privacy_notice.id}
-                                onClick={props.onclose}
-                            >
-                                <span 
-                                    dangerouslySetInnerHTML={{
-                                        __html: privacy_notice.page
-                                    }}
-                                />
-                            </Link>
+                        <div className="megamenu-footer-right">
+                            <div />
+                            <div className="language-preference">
+                                <LanguageComponent />
+                            </div>
+                            <div className="legals">
+                                <Link
+                                    to={visitor_agreement.path}
+                                    id={visitor_agreement.id}
+                                    onClick={props.onclose}
+                                >
+                                    <span
+                                        dangerouslySetInnerHTML={{
+                                            __html: visitor_agreement.page
+                                        }}
+                                    />
+                                </Link>
+                                <Link
+                                    to={privacy_notice.path}
+                                    id={privacy_notice.id}
+                                    onClick={props.onclose}
+                                >
+                                    <span
+                                        dangerouslySetInnerHTML={{
+                                            __html: privacy_notice.page
+                                        }}
+                                    />
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </div>
