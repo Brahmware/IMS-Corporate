20349e20e1e8ae231702124896d0259a345d652e
Dharmesh Patel <dp837533@gmail.com>
2022-06-23 22:38:18 +0530

    professional learning page

---
+++ src/containers/professional-learning/container-5/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CardOverRibbonImageContainer from '../../common/card-over-ribbon-image-container'
+
+const ProfessionalLearningContainerFive = ({ data }) => {
+    return (
+        <div className='professional-learning-container-container-4'>
+            <CardOverRibbonImageContainer cardribbondata={data[0]} />
+        </div>
+    )
+}
+
+export default ProfessionalLearningContainerFive
