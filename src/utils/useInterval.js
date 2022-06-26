f8d97eabca23752a8ec60f01ef9a8e1c3ac78a01
Sanchayan Bhunia <sanchayan.bhunia@hotmail.com>
2022-06-26 15:37:42 +0200

    flashcard infinite loading bug fixed

---
+++ src/utils/useInterval.js
@@ -0,0 +1,29 @@
+import { useRef, useEffect, useState } from 'react';
+
+
+const useInterval = (callback, delay) => {
+    const stableCallback = useRef(callback);
+    
+    useEffect(() => {
+        stableCallback.current = callback;
+    }, [callback]);
+    
+    useEffect(() => {
+        
+        let isCancelled = false;
+        const tick = () => stableCallback.current();
+        
+        if (typeof delay !== 'number') return;
+        
+        const t = setInterval(() => {
+            if(!isCancelled) tick();
+        }, delay);
+
+        return () => {
+            clearInterval(t);
+            isCancelled = true;
+        };
+    }, [delay]);
+}
+
+export default useInterval;
