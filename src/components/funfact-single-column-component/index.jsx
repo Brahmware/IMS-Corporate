77e46ab9cdf1d2249f69e54d2ab19451e860b31c
sanchayan721 <sanchayan.bhunia@hotmail.com>
2023-07-15 00:14:06 +0200

    fixed tags showing problem

--- src/components/funfact-single-column-component/index.jsx
+++ src/components/funfact-single-column-component/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import HollowButton from '../buttons/HollowButton'
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
-const FunfactSingleColumnComponent = ( {columnKey, count, cardtitle, cardcontent, path} ) => {
+const FunfactSingleColumnComponent = ({ columnKey, count, cardtitle, cardcontent, path }) => {
   return (
     <div
       className='column-card'
@@ -11,17 +11,17 @@ const FunfactSingleColumnComponent = ( {columnKey, count, cardtitle, cardcontent
       data-aos-delay={300 + columnKey * 300}
     >
       <div className="count">
-        <span>{count}</span>
+        <span dangerouslySetInnerHTML={{ __html: count }} />
       </div>
       <div className={"card-divider"} />
       <div className="card-title">
-        <span>{cardtitle}</span>
+        <span dangerouslySetInnerHTML={{ __html: cardtitle }} />
       </div>
       <div className="card-content">
-        <span>{cardcontent}</span>
+        <span dangerouslySetInnerHTML={{ __html: cardcontent }} />
       </div>
       <div className="card-button">
-        <Link to={!path?"/":path}><HollowButton class={"gray-button"} text={"DISCOVER"} /></Link>
+        <Link to={!path ? "/" : path}><HollowButton class={"gray-button"} text={"DISCOVER"} /></Link>
       </div>
     </div>
   )
